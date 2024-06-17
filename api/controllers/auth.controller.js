2b5afc98c806101df94d8f1e3bc1f7e579b6ecab
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-06-18 00:14:45 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/users.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import { cannotHaveAUsernamePasswordPort } from "whatwg-url";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,5 +16,35 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     //next(errorHandler(300,"Something went wrong")) //this way we can pass the customed error
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(401, "User Not Found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    //create a token to have user info(this should be unique so im using ID which auto created by the database)
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    //removing password from the client side. it should not be sent
+    const { password: hashPassword, ...rest } = validUser._doc;
+
+    //next we're going to save that token in to a cookie
+    //set a timer to how long cookie should be stay
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
