1875e27b3c9765a6a157678bed1172a4fa95a66f
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-06-19 15:52:16 +0530

    add preproduction changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/users.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,9 +18,14 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json()); //allows json inputs as our inputs
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
